ada2189e01ec36ba659debfe6c334ebdafcfbefb
aaron <doctordigart@gmail.com>
2016-09-24 15:01:02 -0700

    aaron fucked up

--- Assets/ImportedMeshes/SLK/SciFiLevelKit/Scripts/Worldmap.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Worldmap : MonoBehaviour {
-	void Update() {
-		transform.Rotate(Vector3.up*3 * Time.deltaTime);
-	//	transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
-	}
-}
---
+++ Assets/scripts/BodyDisply.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyDisply : MonoBehaviour {
+
+	public GameObject body;
+	public Renderer[] renderers;
+	public void TurnOnOrgans () {
+		
+		renderers = body.GetComponentsInChildren<Renderer>( );
+		foreach (Renderer r in renderers) {
+			r.enabled = false;
+		}
+		Debug.Log(renderers.Length);
+	}
+}
